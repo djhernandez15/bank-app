971f40222233b631854555f4996fb1e2b7fb94bd
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-04-25 09:48:47 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const massive = require("massive");
+const express = require("express");
+const session = require("express-session");
+const { CONNECTION_STRING, SESSION_SECRET, SERVER_PORT } = process.env;
+
+const app = express();
+
+massive(CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+    console.log("Database connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+  })
+);
+
+
+app.listen(SERVER_PORT, () => console.log(`Listening on Port ${SERVER_PORT}`));
